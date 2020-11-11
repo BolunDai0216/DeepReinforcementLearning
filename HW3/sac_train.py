db94df8d6e22f70e311c1760c27a470116b94b33
Bolun Dai <bd1555@nyu.edu>
2020-11-11 12:53:57 -0500

    added polyak averaging, tensorboard and model saving

--- HW3/sac_train.py
+++ HW3/sac_train.py
@@ -21,10 +21,20 @@ class SACAgent:
         self.max_iter = config.max_iter
         self.log_freq = config.log_freq
         self.buffer = ReplayBuffer(self.config)
+        self.polyak_constant = config.polyak_constant
 
     def train(self, render=False):
         train_log_dir = "logs/sac/" + self.stamp + "/train"
         train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+        update_counter = 0
+
+        # Assign eval_net weights to target_net
+        self.sac.critic1_target.net.set_weights(self.sac.critic1_eval.net.get_weights())
+        self.sac.critic2_target.net.set_weights(self.sac.critic2_eval.net.get_weights())
+
+        # Placeholder of loss
+        actor_loss = None
+        critic_loss = None  
 
         for episode in range(self.iter_num):
             cummulative_reward = 0
@@ -59,19 +69,64 @@ class SACAgent:
                 step_num += 1
                 if step_num >= self.max_iter:
                     break
+                
+                # Update model weights
+                if update_counter >= self.config.update_threshold:
+                    if update_counter % self.config.update_freq:
+                        actor_loss, critic_loss = self.train_step()
+                update_counter += 1
 
-            set_trace()
+            print("Iteration: {}, Reward: {}".format(
+            episode, cummulative_reward))
+
+            # Log to TensorBoard
+            if actor_loss is not None:
+                with train_summary_writer.as_default():
+                    tf.summary.scalar("actor_loss_value",
+                                    actor_loss, step=episode)
+                    tf.summary.scalar("critic_loss_value",
+                                    critic_loss, step=episode)
+                    tf.summary.scalar("reward", cummulative_reward, step=episode)
+
+            # Save model
+            if (episode + 1) % self.log_freq == 0:
+                names = ["actor", "critic1_eval", "critic1_target", "critic2_eval", "critic2_target"]
+                nets = [self.sac.actor, self.sac.critic1_eval, self.sac.critic1_target, self.sac.critic2_eval, self.sac.critic2_target]
+                for name, net in zip(names, nets):
+                    filename = "models/{}/{}_{}".format(
+                        name, self.stamp, episode + 1)
+                    net.net.save(filename)
+                    print("Model of {} saved at {}".format(name, filename))
 
     def train_step(self):
-        pass
+        actor_loss = self.opt_actor()
+        critic_loss = self.opt_critic()
+
+        critics = [[self.sac.critic1_eval, self.sac.critic1_target], 
+                   [self.sac.critic2_eval, self.sac.critic2_target]]
+
+        # Polyak Averaging
+        for nets in critics:
+            eval_weights = nets[0].net.get_weights()
+            target_weights = nets[1].net.get_weights()
+            update_weights = []
+            for eval_weight, target_weight in zip(eval_weights, target_weights):
+                update_weights.append(
+                    self.polyak_constant * eval_weight
+                    + (1 - self.polyak_constant) * target_weight
+                )
+            nets[1].net.set_weights(update_weights)
+        return actor_loss, critic_loss
 
     @tf.function
     def opt_actor(self):
-        pass
+        loss_value = 0
+        return loss_value
 
     @tf.function
     def opt_critic(self):
-        pass
+        loss_value = 0
+        return loss_value
 
     def test(self):
         pass
